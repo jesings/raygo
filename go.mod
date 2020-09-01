aefe58f8968aafb337fb15b6cf5e2189a47884a1
jesings <34111484+jesings@users.noreply.github.com>
2020-09-01 17:18:16 -0500

    More logic, reorg

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module newgraph
 
 go 1.15
+
+require github.com/rkusa/gm v0.0.0-20160203133140-acc6b6d0b64a
--- logic/trays.go
+++ logic/trays.go
@@ -1,5 +1,9 @@
 package logic
 
+import (
+    "github.com/rkusa/gm/math32"
+)
+
 type Vec3 struct {
     x, y, z float32
 }
@@ -23,3 +27,33 @@ func (v0 Vec3) Add(v1 Vec3) Vec3 {
 func (v0 Vec3) Sub(v1 Vec3) Vec3 {
     return Vec3{v0.x - v1.x, v0.y - v1.y, v0.z - v1.z}
 }
+
+func (v0 Vec3) Neg() Vec3 {
+    return Vec3{-v0.x, -v0.y, -v0.z}
+}
+
+func (v0 Vec3) LenSq() float32 {
+    return v0.Dot(v0)
+}
+
+func (v0 Vec3) Len() float32 {
+    return math32.Sqrt(v0.LenSq())
+}
+
+func (v0 Vec3) Norm() Vec3 {
+    nor := v0.LenSq()
+    if(nor > 0.0) {
+        inor := 1.0 / math32.Sqrt(nor)
+        return v0.Scale(inor)
+    }
+    return v0
+}
+
+func (v0 Vec3) Cross(v1 Vec3) Vec3 {
+    return Vec3{
+        v0.y * v1.z - v1.y * v0.z,
+        v1.x * v0.z - v0.x * v1.z,
+        v0.x * v1.y - v1.x * v0.y,
+    }
+}
+
